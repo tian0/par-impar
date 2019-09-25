84000bfc6fa4e3d03b5a7f0b0ceeed6aaf565861
tian0 <sebas.aspe.von@gmail.com>
2019-09-25 15:27:04 -0700

    changes to app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     for (i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
-            res.write(`<p>` + i + `. Soy par</p>`)
+            res.write(`<p>` + i + ` Soy Par!</p>`)
         } else {
-            res.write(`<p>` + i + `. Soy impar</p>`)
+            res.write(`<p>` + i + ` Soy Impar!</p>`)
         }
     }
     res.end()
